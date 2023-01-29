b213ab7248d4faf78141d92666c0747b063d4022
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-01-29 21:28:33 +0200

    commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  bodyEl: document.querySelector('body'),
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
+let timerId;
+
+refs.bodyEl.addEventListener('click', chColor);
+
+function chColor(event) {
+  if (event.target === refs.btnStart) {
+    setInterval(colorBody, 1000);
+    refs.btnStart.disabled = true;
+    refs.btnStop.disabled = false;
+  }
+  if (event.target === refs.btnStop) {
+    clearInterval(timerId);
+    refs.btnStart.disabled = false;
+    refs.btnStop.disabled = true;
+  }
+}
+
+function colorBody() {
+  refs.bodyEl.style.backgroundColor = getRandomHexColor();
+}
